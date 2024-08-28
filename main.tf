19ce3a6993df206ad2b2b6157e0009750803bd26
unknown <arati@gmail.com>
2024-08-29 00:30:58 +0530

    modified

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "rgblock" {
   name= "aratirg"
   location = "west us"
 }
+resource "azurerm_resource_group" "rgblock" {
+  name= "rg"
+  location = "Central India"
+}
